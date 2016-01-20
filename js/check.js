ece8f0474c7334846d66f13fbce2cd0dd430d708
Сергей Попов <tovarov.piter@gmail.com>
2016-01-20 20:51:03 +0300

    Revert "Замена стандартных if на один switch"
    This reverts commit 90f2d881a1e2eba37dc81062c95465ef9069f8aa.

--- js/check.js
+++ js/check.js
@@ -1,37 +1,31 @@
 'use strict';
 
 function getMessage(a, b) {
-  switch(typeof a) {
-    case 'boolean':
-      if (a === true) {
-        return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
-      } else {
-        return 'Переданное GIF-изображение не анимировано';
-      }
-      break;
+  if ( typeof a === 'boolean') {
+    if (a === true) {
+      return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+    } else {
+      return 'Переданное GIF-изображение не анимировано';
+    }
+  }
 
-    case 'number':
-      return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' аттрибутов';
-      break;
+  if ( typeof a === 'number') {
+    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' аттрибутов';
+  }
 
-    case 'object':
-      var i = 0, sum = 0;
+  if ( typeof a === 'object') {
+    var i = 0, sum = 0;
 
-      if ( typeof b === 'object') {
-        for ( i = 0; i < a.length; i++) {
-          sum = sum + (a[i] * b[i]);
-        }
-        return 'Общая площадь артефактов сжатия: ' + sum + ' пикселей';
-      }
-
+    if ( typeof b === 'object') {
       for ( i = 0; i < a.length; i++) {
-        sum = sum + a[i];
+        sum = sum + (a[i] * b[i]);
       }
-      return 'Количество красных точек во всех строчках изображения: ' + sum;
-      break;
+      return 'Общая площадь артефактов сжатия: ' + sum + ' пикселей';
+    }
 
-    default:
-      return 'Введенный параметр не соответвует ни одному из стандартных обработчиков.';
-      break;
+    for ( i = 0; i < a.length; i++) {
+      sum = sum + a[i];
+    }
+    return 'Количество красных точек во всех строчках изображения: ' + sum;
   }
 }
