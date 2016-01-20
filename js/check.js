90f2d881a1e2eba37dc81062c95465ef9069f8aa
Сергей Попов <tovarov.piter@gmail.com>
2016-01-20 15:25:45 +0300

    Замена стандартных if на один switch

--- js/check.js
+++ js/check.js
@@ -1,31 +1,37 @@
 'use strict';
 
 function getMessage(a, b) {
-  if ( typeof a === 'boolean') {
-    if (a === true) {
-      return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
-    } else {
-      return 'Переданное GIF-изображение не анимировано';
-    }
-  }
-
-  if ( typeof a === 'number') {
-    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' аттрибутов';
-  }
-
-  if ( typeof a === 'object') {
-    var i = 0, sum = 0;
-
-    if ( typeof b === 'object') {
-      for ( i = 0; i < a.length; i++) {
-        sum = sum + (a[i] * b[i]);
+  switch(typeof a) {
+    case 'boolean':
+      if (a === true) {
+        return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+      } else {
+        return 'Переданное GIF-изображение не анимировано';
       }
-      return 'Общая площадь артефактов сжатия: ' + sum + ' пикселей';
-    }
+      break;
 
-    for ( i = 0; i < a.length; i++) {
-      sum = sum + a[i];
-    }
-    return 'Количество красных точек во всех строчках изображения: ' + sum;
+    case 'number':
+      return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' аттрибутов';
+      break;
+
+    case 'object':
+      var i = 0, sum = 0;
+
+      if ( typeof b === 'object') {
+        for ( i = 0; i < a.length; i++) {
+          sum = sum + (a[i] * b[i]);
+        }
+        return 'Общая площадь артефактов сжатия: ' + sum + ' пикселей';
+      }
+
+      for ( i = 0; i < a.length; i++) {
+        sum = sum + a[i];
+      }
+      return 'Количество красных точек во всех строчках изображения: ' + sum;
+      break;
+
+    default:
+      return 'Введенный параметр не соответвует ни одному из стандартных обработчиков.';
+      break;
   }
 }
