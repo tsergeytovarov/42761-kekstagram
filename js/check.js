ae7821764afa3ebe4eded0bb8c3d583303575830
Сергей Попов <tovarov.piter@gmail.com>
2016-01-20 20:52:17 +0300

    Упростил выражение

--- js/check.js
+++ js/check.js
@@ -2,7 +2,7 @@
 
 function getMessage(a, b) {
   if ( typeof a === 'boolean') {
-    if (a === true) {
+    if (a) {
       return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
     } else {
       return 'Переданное GIF-изображение не анимировано';
